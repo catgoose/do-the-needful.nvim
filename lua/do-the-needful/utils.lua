afd0ddf6cafb2cc01c213484839f81b8aa63c8de
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-02-27 06:17:00 -0600

    feat: creates deep_copy util function

--- lua/do-the-needful/utils.lua
+++ lua/do-the-needful/utils.lua
@@ -1,6 +1,20 @@
 Utils = {}
 
-function Utils.ident_str(indent_n, str)
+Utils.deep_copy = function(orig)
+	local t = type(orig)
+	local copy
+	if t == "table" then
+		copy = {}
+		for k, v in pairs(orig) do
+			copy[k] = Utils.deep_copy(v)
+		end
+	else
+		copy = orig
+	end
+	return copy
+end
+
+Utils.indent_str = function(indent_n, str)
 	return ("\t"):rep(indent_n) .. str
 end
 
